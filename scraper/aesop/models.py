4cafd69c5f9b22860811f182230d993b13ab2b06
Sven Zetterlund <39314163+zetterlund@users.noreply.github.com>
2019-03-09 21:12:15 -0600

    upload scraper files

---
+++ scraper/aesop/models.py
@@ -0,0 +1,34 @@
+from sqlalchemy import create_engine, Column, Table, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import (Integer, SmallInteger, String, Date, DateTime, Float, Boolean, Text, LargeBinary)
+
+from scrapy.utils.project import get_project_settings
+
+DeclarativeBase = declarative_base()
+
+def db_connect():
+    return create_engine(get_project_settings().get("CONNECTION_STRING"))
+
+def create_table(engine):
+    DeclarativeBase.metadata.create_all(engine)
+
+
+class ListingDB(DeclarativeBase):
+    __tablename__ = "listing"
+
+    id = Column(Integer, primary_key=True)
+    aesop_id = Column('aesop_id', Integer)
+    teacher = Column('teacher', String(256))
+    position = Column('position', String(128))
+    subject = Column('subject', String(128))
+    campus = Column('campus', String(256))    
+    begin_date = Column('begin_date', DateTime)
+    end_date = Column('end_date', DateTime)    
+    multiday = Column('multiday', Boolean)    
+    fullday = Column('fullday', String(128))
+    notes = Column('notes', Text)
+    date_posted = Column('date_posted', DateTime)
+    date_removed = Column('date_removed', DateTime)
+    language = Column('language', String(64))
+    grade = Column('grade', String(64))
+    notification_sent = Column('notification_sent', Boolean)
---
+++ scraper/aesop/spiders/listings.py
@@ -0,0 +1,173 @@
+# -*- coding, utf-8 -*-
+import scrapy
+from aesop.items import ListingItem
+
+import os
+import re
+import time
+import random
+from urllib.request import urlretrieve
+import hashlib
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from scrapy.utils.project import get_project_settings
+
+
+
+def get_fields(listing, job, field_dict):
+    for i in range(len(field_dict)):
+        field = list(field_dict.keys())[i]
+        extract = list(field_dict.values())[i]
+        attribute = extract[1]
+        if attribute == 'text':
+            try:
+                value = job.find_element_by_xpath(extract[0]).text
+            except:
+                value = ""
+            finally:
+                listing[field] = value
+        else:
+            # For any non-text attributes, add logic here:
+            listing[field] = ""
+    return listing
+
+
+
+class ListingsSpider(scrapy.Spider):
+    name = 'listings'
+    start_urls = ['http://aesoponline.com']
+
+
+    def __init__(self):
+        self.chrome_options = Options()
+        self.chrome_options.add_argument("--headless") # Keep this to ensure Chrome runs properly
+        self.chrome_options.add_argument('--user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"')
+        # self.driver = webdriver.Chrome(executable_path=os.path.abspath("/home/sven/scrape/chromedriver237"), chrome_options=self.chrome_options)
+
+
+
+    def parse(self, response):
+
+        driver1 = webdriver.Chrome(executable_path=os.path.abspath("/home/sven/scrape/chromedriver237"), chrome_options=self.chrome_options)
+
+        try:
+            driver1.get(response.url)
+            time.sleep(3)
+
+            usr = driver1.find_element_by_xpath("//form//input[@name='Username']")
+            pwd = driver1.find_element_by_xpath("//form//input[@name='Password']")
+            username = get_project_settings().get("ACCOUNT_LOGIN")
+            password = get_project_settings().get("ACCOUNT_PASSWORD")
+            usr.send_keys(username)
+            pwd.send_keys(password)
+
+            logbtn = driver1.find_element_by_xpath("//button[@type='submit']")
+            logbtn.click()
+            # time.sleep(10)
+
+
+
+            try:
+                element = WebDriverWait(driver1, 30).until(
+                    EC.presence_of_element_located((By.ID, "availableJobs"))
+                )
+            finally:
+                pass
+
+
+
+            ''' Close popup window if it exists '''
+            try:
+                time.sleep(1)
+                close_button = driver1.find_element_by_xpath("//button[@type='button' and contains(@class, 'titlebar-close')]")
+                close_button.click()
+                time.sleep(1)
+            finally:
+                pass
+
+
+
+            # ''' Take Screenshot '''
+            # driver1.save_screenshot('screenshots/test1.png')
+
+
+
+            ''' Extract info from each job '''
+            jobs = driver1.find_elements_by_xpath("//div[@id='availableJobs']//tbody[contains(@class, 'job')]")
+
+
+
+            # ''' Extract only a few jobs for testing '''
+            # jobs = jobs[:10]
+
+
+
+            for job in jobs:
+
+                listing = ListingItem()
+
+                field_dict = {
+                'teacher': (".//span[@class='name']", 'text'),
+                'title': (".//span[@class='title']", 'text'),
+                'times': (".//td[@class='times']", 'text'),
+                'fullday': (".//span[@class='durationName']", 'text'),
+                'campus': (".//div[@class='locationName']", 'text'),
+                }
+
+                get_fields(listing, job, field_dict)
+
+
+
+                listing['aesop_id'] = str(job.get_attribute("id"))
+
+                try:
+	                if "multiday" in job.get_attribute("class"):
+	                    listing['multiday'] = True
+	                    listing['begin_date'] = job.find_element_by_xpath(".//td[@class='date']//span[@class='itemDate']").text
+	                    listing['end_date'] = job.find_element_by_xpath(".//td[@class='date']//span[@class='multiEndDate']").text
+	                else:
+	                    listing['multiday'] = False
+	                    listing['begin_date'] = job.find_element_by_xpath(".//td[@class='date']//span[@class='itemDate']").text
+	                    listing['end_date'] = listing['begin_date']
+                except:
+                	listing['multiday'] = "ERROR_FOUND1"
+                	listing['begin_date'] = "ERROR_FOUND1"
+                	listing['end_date'] = "ERROR_FOUND1"
+
+                try:
+                    job.find_element_by_xpath(".//a[contains(@class, 'hasNotes')]").click()
+                    time.sleep(.8)
+                
+                    note_content = driver1.find_element_by_xpath("//div[@class='ui-dialog-content ui-widget-content']").text
+                    listing['notes'] = note_content
+                    close_button = driver1.find_element_by_xpath("//button[@type='button' and contains(@class, 'titlebar-close')]")
+                    close_button.click()
+                    time.sleep(.5)
+
+                except Exception as e:
+                    listing['notes'] = ""
+
+
+
+                yield listing
+
+
+                # try:
+                #     flytitle = driver1.find_element_by_xpath("//span[@class='flytitle-and-title__flytitle']").text
+                #     article['flytitle'] = flytitle
+                # except:
+                #     article['flytitle'] = None
+                
+
+                # try:
+                #     yield article
+                # except:
+                #     pass
+
+
+        finally:
+            driver1.close()
