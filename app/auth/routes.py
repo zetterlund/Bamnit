2adccd69a3a821c22c879355134fc50a72810872
Sven Zetterlund <39314163+zetterlund@users.noreply.github.com>
2020-06-08 09:51:19 -0500

    Fix 'Reset Password' functionality

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -60,7 +60,7 @@ def reset_password_request():
         return redirect(url_for('main.index'))
     form = ResetPasswordRequestForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()
+        user = User.query.filter_by(email=form.email.data).first()       
         if user:
             send_password_reset_email(user)
         flash(
